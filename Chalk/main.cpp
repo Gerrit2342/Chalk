a63fdd8a20a1f61e30adea9c0bfbe5618795afc2
Gerrit Albert <Gerrit@MacBook-Pro.local>
2017-04-06 14:52:37 +0200

    Initial Commit

---
+++ Chalk/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chalk
+//
+//  Created by Gerrit Albert on 06.04.17.
+//  Copyright © 2017 Gerrit Albert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
